92f9a35138ca135cbd0a74b6de7d4c858ab1833a
iruy <yuri_matteus@hotmail.com>
2025-09-14 14:40:47 -0300

    create: herosection

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,14 @@
+export default function HeroSection() {
+  return (
+    <section
+      id="home"
+      className="min-h-screen flex items-center justify-center relative overflow-hidden"
+    >
+        
+    <div className="container mx-auto px-4 lg:px-8 relative z-10">
+        aaaaaaaaaaaaaaaa
+    </div>
+
+    </section>
+  );
+}
